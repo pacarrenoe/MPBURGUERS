b45ccdec5124c211b70c1549f87ffdaf13dc18c7
MarielaHernandez <mari.hernandezr@alumnos.duoc.cl>
2020-11-08 15:58:42 +0000

    3.Creacion de ambiente para base de datos y modificacion de templates (cuerpo)

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from app.models import Producto, Cliente, Pedido
+
+# Register your models here.
+
+
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Cliente)
+admin.site.register(Pedido)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models 
+
+# Create your models here.
+
+class Producto (models.Model):
+    CodigoP=models.AutoField(primary_key=True, verbose_name="Codigo -")
+    NombreP=models.CharField(max_length=35, verbose_name="Nombre del producto -")
+    PrecioP=models.IntegerField(verbose_name="Precio -")
+    Descripcion=models.TextField(max_length=500, blank=True, null=True, verbose_name="Descripcion -")
+    Imagen=models.ImageField(upload_to="productos", null=True)
+
+
+class Cliente (models.Model):   
+    RutC=models.AutoField(primary_key=True, verbose_name="Rut -")
+    Nombres=models.CharField(max_length=70, verbose_name="Nombre ")
+    Apellidos=models.CharField(max_length=70, verbose_name="Apellido -")
+    Direccion=models.CharField(max_length=70, verbose_name="Direccion -")
+    Fono=models.IntegerField( verbose_name="Numero contacto -")
+    Email=models.EmailField(blank=True, null=True, verbose_name="Email -")    
+    
+    def DatosCliente(self):
+        cadena = "{0} {1} {2}"
+        return cadena.format(self.Nombres, self.Apellidos, self.Fono)
+
+    def __str__ (self):
+        return self.DatosCliente()    
+
+class Pedido (models.Model): 
+    NumPedido=models.AutoField(primary_key=True, verbose_name="Numero de pedido -")
+    Cliente=models.ForeignKey(Cliente, null=False, blank=False, on_delete=models.CASCADE, verbose_name="Cliente -")
+    Producto=models.ForeignKey(Producto, null=False, blank=False, on_delete=models.CASCADE, verbose_name="Producto -")
+    Estado=models.BooleanField(verbose_name="Estado -")
+    Fecha=models.DateTimeField(auto_now_add=True, verbose_name="Fecha de pedido -")
+
+    def Final(self):
+        cadena = "{0} {1} {2}"
+        return cadena.format(self.Cliente, self.NumPedido, self.Estado)
+
+    def __str__ (self):
+        return self.Final()    
+                          
+                      
